73d7feba82ea42e2a44eb0a230a30e07dfbc45c5
tranducnam01 <namdzxy01@gmail.com>
2025-12-23 15:09:02 +0700

    Da sua loi xung dot LeadReport

--- app/Models/LeadReport.php
+++ app/Models/LeadReport.php
@@ -68,17 +68,9 @@ class LeadReport extends Model
     public static function findByIdWithDetails($id)
     {
         $db = self::db();
-<<<<<<< Updated upstream
-        $sql = "SELECT lr.*, 
-                       u.ho_ten as ten_nguoi_gui, 
-                       u.so_dien_thoai as sdt_nguoi_gui,
-                       c.ho_ten as ten_khach, 
-                       c.so_dien_thoai as sdt_khach,
-                       c.nam_sinh as nam_sinh_khach,
-                       c.cccd as cccd_khach
-=======
+
         $sql = "SELECT lr.*, c.*, u.ho_ten AS sender_name, u.so_dien_thoai AS sender_phone
->>>>>>> Stashed changes
+
                 FROM lead_reports lr
                 LEFT JOIN users u ON lr.user_id = u.id
                 LEFT JOIN customers c ON lr.customer_id = c.id
